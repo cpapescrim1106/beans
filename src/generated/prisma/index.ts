a9bea5e86a78819a2600f854665fc60bf39446e1
cpapescrim1106 <cppape@gmail.com>
2026-01-29 14:56:44 +0000

    fix: add index.ts to re-export Prisma generated types

---
+++ src/generated/prisma/index.ts
@@ -0,0 +1,13 @@
+/* !!! This is a manual re-export file for Prisma generated types. !!! */
+
+// Re-export enums
+export * from "./enums";
+
+// Re-export client
+export * from "./client";
+
+// Re-export models
+export * from "./models";
+
+// Re-export common input types
+export * from "./commonInputTypes";
